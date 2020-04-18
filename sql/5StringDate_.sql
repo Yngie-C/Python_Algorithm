3bc86326df81a1035d573bb694ceb454d0a2f606
Yngie-C <efrks@naver.com>
2020-04-18 09:37:25 +0900

    createSQLs

---
+++ sql/5StringDate_.sql
@@ -0,0 +1,46 @@
+-- 루시와 엘라 찾기
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+        FROM ANIMAL_INS
+        WHERE NAME = 'Lucy' OR
+        NAME = 'Ella' OR
+        NAME = 'Pickle' OR
+        NAME = 'Rogan' OR
+        NAME ='Sabrina' OR
+        NAME ='Mitty';
+
+-- 이름에 el이 들어가는 동물 찾기
+SELECT
+    ANIMAL_ID,
+    NAME
+        FROM ANIMAL_INS
+        WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE='DOG'
+        ORDER BY NAME;
+
+-- 중성화 여부 파악하기
+SELECT
+    ANIMAL_ID,
+    NAME,
+    IF(SEX_UPON_INTAKE LIKE 'NEUTERED %' OR SEX_UPON_INTAKE LIKE 'SPAYED %','O','X') AS 증성화
+        FROM ANIMAL_INS
+        ORDER BY ANIMAL_ID;
+
+-- 오랜 기간 보호한 동물(2)
+SELECT
+    A.ANIMAL_ID,
+    A.NAME
+        FROM ANIMAL_INS A
+        INNER JOIN ANIMAL_OUTS B
+        WHERE A.ANIMAL_ID = B.ANIMAL_ID
+            ORDER BY (B.DATETIME - A.DATETIME) DESC
+            LIMIT 2;
+
+-- DATETIME에서 DATE로 형 변환
+SELECT
+    ANIMAL_ID,
+    NAME,
+    DATE_FORMAT(DATETIME, '%Y-%m-%d') DATEONLY
+        FROM ANIMAL_INS
+        ORDER BY ANIMAL_ID;
